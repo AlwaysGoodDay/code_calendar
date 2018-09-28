d19fe3e523d5cbd9e6ea1f1ec190af48ae2e8faf
dmk <dongmkim@yahoo.com>
2018-09-28 15:53:07 +0200

    Update(plus max days of month)

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -1,5 +1,7 @@
 package Calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,16 @@ public class Calendar {
 		System.out.println(" 8   9  10  11  12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
+		
+		//Find maximum dates of month.
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Type month");
+		int month=scanner.nextInt();
+		
+		int[] maxDays= {31, 28, 31,30,31,30, 31,31,30,31, 30, 31};
+		
+		System.out.printf("Month %d has maximum %d days", month, maxDays[month-1] );
+		scanner.close();
 
 	}
 
